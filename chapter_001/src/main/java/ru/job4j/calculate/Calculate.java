39b44b3b9553e71788709503cc3e2a201b7e34fe
Boris Brakhin <mailto:brakhin@mail.ru>
2018-08-14 22:13:30 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,9 +8,20 @@ public class Calculate {
 	
 	/**
 	* the main method for class Calculate
+	* @param args - args
 	*/	
 	public static void main(String[] args) {
 	
 		System.out.println("Hello World");
 	}	
+	
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+     
+		return "Echo, echo, echo : " + name;
+    }
 }
