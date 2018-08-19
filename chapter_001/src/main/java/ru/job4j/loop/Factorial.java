c779a6e770060117ce681396e9249f2760eb0ace
Boris Brakhin <mailto:brakhin@mail.ru>
2018-08-19 09:51:40 +0300

    5.2. Создать программу вычисляющую факториал

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+/**
+ * @author Boris Brakhin (mailto:brakhin@mail.ru)
+ * @version $Id$
+ * @since 19.08.2018
+ */public class Factorial {
+    /**
+     * Факториал числа
+     * @param value число, факториал которого вычисляется
+     * @return
+     */
+    public int calc(int value) {
+        int result = 1;
+        for (int i = 1; i <= value; i++ ) {
+           result *= i;
+        }
+        return result;
+    }
+}
