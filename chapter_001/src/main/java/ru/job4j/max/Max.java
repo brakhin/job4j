8058208a25d423172e9493457e8ccf0a9e9bbc15
Boris Brakhin <mailto:brakhin@mail.ru>
2018-08-22 19:13:09 +0300

    4.3. Вычисление площади треугольника - 2 + доп. исправления

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,11 +2,11 @@ package ru.job4j.max;
 
 public class Max {
 
-    public static int max(int a, int b) {
+    public int max(int a, int b) {
         return a > b ? a : b;
     }
 
-    public static int max(int a, int b, int c) {
+    public int max(int a, int b, int c) {
         return max(a, max(b, c));
     }
 }
