60e627bb49b3d3dbba134cec14ec19d039666b75
Boris Brakhin <mailto:brakhin@mail.ru>
2018-08-23 19:01:44 +0300

    6.4. Слова начинается с (дополнение)

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,17 +2,12 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
+        boolean result = true;
         for (int i = 0; i < data.length - 1; i++) {
-            if (data[i][i] != data[i + 1][i + 1]) {
-                return false;
+            if (data[i][i] != data[i + 1][i + 1] || data[data.length - i - 1][i] != data[data.length - i - 2][i + 1]) {
+                result = false;
             }
         }
-        for (int i = 0; i < data.length - 1; i++) {
-            if (data[data.length - i - 1][i] != data[data.length - i - 2][i + 1]) {
-                return false;
-            }
-        }
-
-        return true;
+        return result;
     }
 }
