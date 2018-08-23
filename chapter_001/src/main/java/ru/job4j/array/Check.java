ebfd8cc13e40a8e2548c63e3d8b07abde6361606
Boris Brakhin <mailto:brakhin@mail.ru>
2018-08-23 18:53:07 +0300

    3.1. Элементарный калькулятор (дополнение)

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,10 +3,12 @@ package ru.job4j.array;
 public class Check {
     public boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            result &= data[i];
+        for (int i = 1; i < data.length; i++) {
+            result = (data[0] == data[i]);
+            if (result) {
+                break;
+            }
         }
-
         return result;
     }
 }
