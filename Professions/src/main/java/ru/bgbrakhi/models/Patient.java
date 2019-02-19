da10ebe10a3647ffee6b4e24211ef3400d1ef58c
Boris Brakhin <mailto:brakhin@mail.ru>
2019-02-19 19:07:08 +0300

    Professions_commit

---
+++ Professions/src/main/java/ru/bgbrakhi/models/Patient.java
@@ -0,0 +1,14 @@
+package ru.bgbrakhi.models;
+
+public class Patient {
+	private String fio;
+	
+	public void setFio(String fio) { 
+		this.fio = fio;
+	}
+	
+	public String getFio() {
+		return this.fio;
+	}	
+}
+
---
+++ Professions/src/main/java/ru/bgbrakhi/models/Student.java
@@ -0,0 +1,14 @@
+package ru.bgbrakhi.models;
+
+public class Student {
+	private String fio;
+	
+	public void setFio(String fio) { 
+		this.fio = fio;
+	}
+	
+	public String getFio() {
+		return this.fio;
+	}	
+}
+
