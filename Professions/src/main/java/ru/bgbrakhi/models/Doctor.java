17934d636c3d2a5273612568e20d3d5035467658
Boris Brakhin <mailto:brakhin@mail.ru>
2019-02-19 19:07:08 +0300

    Professions_commit

---
+++ Professions/src/main/java/ru/bgbrakhi/models/Doctor.java
@@ -0,0 +1,7 @@
+package ru.bgbrakhi.models;
+
+public class Doctor extends Profession {
+	public void treat(Patient patient) {
+	}
+}
+
---
+++ Professions/src/main/java/ru/bgbrakhi/models/House.java
@@ -0,0 +1,14 @@
+package ru.bgbrakhi.models;
+
+public class House {
+	private String address;
+	
+	public void setAddress(String address) { 
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}	
+}
+
