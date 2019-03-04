722ddd71ef193b0a408f45c4f2b86f05fb30943b
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-05 00:38:12 +0300

    2. Обобщения. 2. Написать программу преобразования List в Map

---
+++ chapter_003/list/src/main/java/ru/job4j/list/User.java
@@ -0,0 +1,17 @@
+package ru.job4j.list;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String city;
+
+    public User(Integer id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+}
