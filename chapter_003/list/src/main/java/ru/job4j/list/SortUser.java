ed250e1077058c62e03aa491c3c31e564fb70de5
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-06 07:35:21 +0300

    3. Сортировка. 2. Сортировка User с использованием Comparator (исправление 2)

--- chapter_003/list/src/main/java/ru/job4j/list/SortUser.java
+++ chapter_003/list/src/main/java/ru/job4j/list/SortUser.java
@@ -10,9 +10,7 @@ public class SortUser {
         Set<User> result = new TreeSet<>(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                if (o1.getAge() < o2.getAge()) return -1;
-                else if (o1.getAge() > o2.getAge()) return 1;
-                else return 0;
+                return Integer.compare(o1.getAge(), o2.getAge());
             }
         });
         result.addAll(users);
