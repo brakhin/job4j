8a46ce64b175838a6138d31197edf4f44a147b31
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-10 22:32:09 +0300

    2. Stream API. 3. Уникальность элементов и сортировка.

--- school/src/main/java/ru/bgbrakhi/stream/Address.java
+++ school/src/main/java/ru/bgbrakhi/stream/Address.java
@@ -1,15 +1,35 @@
 package ru.bgbrakhi.stream;
 
 public class Address {
-    private String city;
-    private String street;
-    private int home;
-    private int apartment;
+    String city;
+    String street;
+    int home;
+    int apartment;
 
     public Address(String city, String street, int home, int apartment) {
         this.city = city;
         this.street = street;
         this.home = home;
         this.apartment = apartment;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Address address = (Address)obj;
+        return this.city.toLowerCase().equals(address.city.toLowerCase()) &&
+               this.street.toLowerCase().equals(address.street.toLowerCase()) &&
+               this.home == address.home &&
+               this.apartment == address.apartment;
+    }
+
+    @Override
+    public int hashCode() {
+        return new StringBuilder().
+                append(city.toLowerCase()).
+                append(street.toLowerCase()).
+                append(home).
+                append(apartment).
+                toString().
+                hashCode();
     }
 }
