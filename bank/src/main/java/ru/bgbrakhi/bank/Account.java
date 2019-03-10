b5b458587a25421918b7d13064ce54f0fde6b19a
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-11 00:27:37 +0300

    2. Stream API. 6. Тестовое задание из модуля коллекции Lite переделать на Stream API.

---
+++ bank/src/main/java/ru/bgbrakhi/bank/Account.java
@@ -0,0 +1,23 @@
+package ru.bgbrakhi.bank;
+
+public class Account {
+
+    double value = 0d;
+    String requisites = "";
+
+    public Account(double value, String requisites) {
+        this.value = value;
+        this.requisites = requisites;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (o != null && getClass() == o.getClass() && this.requisites.equals(((Account)o).requisites));
+    }
+
+    @Override
+    public int hashCode() {
+        return this.requisites.hashCode();
+    }
+
+}
