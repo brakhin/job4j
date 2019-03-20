347bdb4ae6c46aa8e343de65cd294117a05773ea
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-20 22:46:27 +0300

    1. Iterator : 5.1.2. Создать итератор четные числа[#123604] (исправление)

--- Junior_Level/iterator/src/main/java/ru/bgrakhi/iterator/EvenNumbersIterator.java
+++ Junior_Level/iterator/src/main/java/ru/bgrakhi/iterator/EvenNumbersIterator.java
@@ -1,36 +1,40 @@
 package ru.bgrakhi.iterator;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class EvenNumbersIterator implements Iterator {
 
-    private List<Integer> data;
+    private int[] data;
     private int index;
-    private int evenValuesCount;
 
     public EvenNumbersIterator(final int[] data) {
-        this.data = new ArrayList<>();
-        for (int i = 0; i < data.length; i++) {
-            this.data.add(data[i]);
-        }
-        this.data = this.data
-                .stream()
-                .filter(i -> i % 2 == 0)
-                .collect(Collectors.toList());
+        this.data = data;
+        index = getLastEvenIndex(0);
     }
 
     @Override
     public boolean hasNext() {
-        return index < data.size();
+        return index < data.length;
+    }
+
+    private int getLastEvenIndex(int fromIndex) {
+        int result = data.length;
+        for (int i = fromIndex ; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                result = i;
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public Object next() {
-        if (hasNext()) {
-            return data.get(index++);
-        } else {
+        int cur_index = index;
+        index = getLastEvenIndex(cur_index + 1);
+        if (cur_index == data.length) {
             throw new NoSuchElementException();
         }
+        return Integer.valueOf(data[cur_index]);
     }
 }
