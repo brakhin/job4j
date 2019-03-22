0b3c5fc785f5f616045eb18ce7d583553056e083
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-22 22:31:30 +0300

    3. List. 5.3.1. Создать динамический список на базе массива.

---
+++ 2_collections_pro/src/main/java/ru/bgrakhi/list/DynamicContainer.java
@@ -0,0 +1,58 @@
+package ru.bgrakhi.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicContainer<E> implements Iterable<E> {
+
+    private Object[] container;
+    int size = 0;
+    int iteratorIndex = 0;
+    private int modCount = 0;
+
+    private Object[] grow(int sizeDelta) {
+        Object[] result = container;
+        if (size == container.length) {
+            result = Arrays.copyOf(container, container.length + Math.max(1, sizeDelta));
+            modCount++;
+        }
+        return result;
+    }
+
+    public void add(E element) {
+        container = grow(container.length);
+        container[size++] = element;
+        modCount++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) container[index];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return iteratorIndex < container.length;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else if (expectedModCount < modCount)
+                    throw new ConcurrentModificationException();
+                return (E) container[iteratorIndex++];
+            }
+        };
+    }
+}
