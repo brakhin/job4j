d4f81b7976626f15c860a9e538f7703f031286c7
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-22 17:39:44 +0300

    2. Generic. 5.2.1. Реализовать SimpleArray<T> (исправление)

---
+++ 2_collections_pro/src/main/java/ru/bgrakhi/generic/SimpleArray.java
@@ -0,0 +1,51 @@
+package ru.bgrakhi.generic;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SimpleArray<T>  implements Iterable<T> {
+    private Object[] data;
+    private int size;
+
+    public SimpleArray(int initialsize) {
+        this.size = initialsize;
+        data = (T[]) new Object[this.size];
+    }
+
+    private Object[] grow(int sizeDelta) {
+        Object[] result = data;
+        if (size == data.length) {
+            result = Arrays.copyOf(data, data.length + sizeDelta);
+        }
+        return result;
+    }
+
+    public void add(T element) {
+        data = grow(data.length);
+        data[size++] = element;
+    }
+
+    public void set(T element, int index) {
+        if (index < size) {
+            data[index] = element;
+        }
+    }
+
+    public void remove(int index) {
+        System.arraycopy(data, index + 1, data, index, data.length - index - 1);
+        data = Arrays.copyOf(data, data.length - 1);
+        size--;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T)data[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return (Iterator<T>) Arrays.asList(data).iterator();
+    }
+}
