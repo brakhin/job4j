f55b2aec54c71c595d0bc83d19d9d6f2320a1ff3
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-23 22:06:23 +0300

    5. Мар. 8. Реализовать собственную структуру данных - HashMap

---
+++ 2_collections_pro/src/main/java/ru/bgrakhi/map/CustomHashMap.java
@@ -0,0 +1,128 @@
+package ru.bgrakhi.map;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class CustomHashMap<K, V> {
+
+    public static class Entry<K_, V_> {
+        private K_ key;
+        private V_ value;
+
+        public Entry(K_ key, V_ value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K_ getKey() {
+            return key;
+        }
+
+        public V_ getValue() {
+            return value;
+        }
+    }
+
+    LinkedList<Entry> table;
+    private int tableLength;
+
+    public CustomHashMap() {
+        table = new LinkedList<>();
+        reboundTable(tableLength);
+    }
+
+    static int indexFor(int h, int length) {
+        return h & (length - 1);
+    }
+
+    static final int hash(Object key) {
+        int h = key.hashCode();
+        return (key == null) ? 0 : (h) ^ (h >>> 16);
+    }
+
+    private void reboundTable(int newTableLength) {
+        LinkedList<Entry> newTable = new LinkedList<>();
+
+        for (int i = 0; i < newTableLength; i++) {
+            newTable.add(null);
+        }
+        tableLength = newTableLength;
+
+        LinkedList<Entry> oldTable = table;
+        table = newTable;
+
+        for (Entry<K, V> entry: oldTable) {
+            if (entry != null) {
+                int index = getIndex(entry.getKey());
+                setEntry(entry.getKey(), entry.getValue(), index);
+            }
+        }
+    }
+
+    public boolean setEntry(K key, V value, int index) {
+        boolean result = false;
+
+        result = (table.get(index) == null);
+        if (result) {
+            Entry<K, V> entry = new Entry<>(key, value);
+            table.set(index, entry);
+        }
+        return result;
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : indexFor(hash(key), tableLength);
+    }
+
+    public boolean insert(K key, V value) {
+        int index = getIndex(key);
+
+        if (index >= tableLength) {
+            reboundTable(tableLength * 2);
+            index = getIndex(key);
+        }
+        return setEntry(key, value, index);
+    }
+
+    public V get(K key) {
+        int index = getIndex(key);
+        Entry<K, V> entry = table.get(index);
+        return entry == null ? null : entry.getValue();
+    }
+
+    boolean delete(K key) {
+        int index = getIndex(key);
+        boolean result = (table.get(index) != null);
+        table.remove(index);
+        return result;
+    }
+
+    public Iterator<Entry> getIterator() {
+
+        return new Iterator<Entry>() {
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                for (int i = index; i < tableLength; i++) {
+                    if (table.get(i) != null) {
+                        result = true;
+                        index = i;
+                        break;
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public Entry next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table.get(index++);
+            }
+        };
+    }
+}
