6f00316c5ddc4e004856c6d734b5c341352f5d27
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-24 08:05:35 +0300

    Сортировка департаментов

--- 1_depsort/src/main/java/ru/bgbrakhi/depsort/DepRef.java
+++ 1_depsort/src/main/java/ru/bgbrakhi/depsort/DepRef.java
@@ -8,48 +8,12 @@ public class DepRef {
 
     private List<String> data;
 
-    private Comparator<String> comparatorMasterAsc;
-    private Comparator<String> comparatorMasterDesc;
-    private Comparator<String> comparatorChild;
-
     public DepRef() {
         data = new ArrayList<>();
+    }
 
-        comparatorMasterAsc = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String dep1 = o1.substring(0, Math.max(0, o1.indexOf("\\")));
-                if (dep1.isEmpty()) {
-                    dep1 = o1;
-                }
-                String dep2 = o2.substring(0, Math.max(0, o2.indexOf("\\")));
-                if (dep2.isEmpty()) {
-                    dep2 = o2;
-                }
-                return dep1.compareTo(dep2);
-            }
-        };
-        comparatorMasterDesc = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String dep1 = o1.substring(0, Math.max(0, o1.indexOf("\\")));
-                if (dep1.isEmpty()) {
-                    dep1 = o1;
-                }
-                String dep2 = o2.substring(0, Math.max(0, o2.indexOf("\\")));
-                if (dep2.isEmpty()) {
-                    dep2 = o2;
-                }
-                return dep2.compareTo(dep1);
-            }
-        };
-        comparatorChild = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        };
-
+    public List<String> getData() {
+        return data;
     }
 
     public void addDep(String department) {
@@ -58,7 +22,7 @@ public class DepRef {
         }
     }
 
-    private void validateData() {
+    public void validateData() {
         for (int i = 0; i < data.size(); i++) {
             String dep = data.get(i);
             int slashIndex = dep.lastIndexOf("\\");
@@ -69,16 +33,6 @@ public class DepRef {
                 }
             }
         }
-    }
-
-    public void sortAsc() {
-        validateData();
-        data.sort(comparatorMasterAsc.thenComparing(comparatorChild));
-    }
-
-    public void sortDesc() {
-        validateData();
-        data.sort(comparatorMasterDesc.thenComparing(comparatorChild));
     }
 
     public String toString() {
