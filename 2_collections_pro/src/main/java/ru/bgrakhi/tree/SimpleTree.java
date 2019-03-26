94970ab56a434ba912f4b7a034f86a0cfcb7c9c4
Boris Brakhin <mailto:brakhin@mail.ru>
2019-03-26 21:27:34 +0300

    6. Tree. 1. Создать элементарную структуру дерева

---
+++ 2_collections_pro/src/main/java/ru/bgrakhi/tree/SimpleTree.java
@@ -0,0 +1,20 @@
+package ru.bgrakhi.tree;
+
+import java.util.*;
+
+public interface SimpleTree<E extends Comparable<E>> extends Iterable<E> {
+    /**
+     * Добавить элемент child в parent.
+     * Parent может иметь список child.
+     * @param parent parent.
+     * @param child child.
+     * @return
+     */
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+}
+
+
+
+
