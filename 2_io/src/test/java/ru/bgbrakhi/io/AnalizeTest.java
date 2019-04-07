9216861cdef15af44f7255ead5b4d67070ef9a76
Boris Brakhin <mailto:brakhin@mail.ru>
2019-04-07 12:15:43 +0300

    2. Анализ доступности сервера. [#859]

---
+++ 2_io/src/test/java/ru/bgbrakhi/io/AnalizeTest.java
@@ -0,0 +1,24 @@
+package ru.bgbrakhi.io;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringJoiner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AnalizeTest {
+    @Test
+    public void testWork() throws IOException {
+        Analize analize = new Analize("server.log", "unavailable.csv");
+        analize.work();
+        try (BufferedReader reader = new BufferedReader(new FileReader("unavailable.csv"))) {
+            StringJoiner builder = new StringJoiner(System.getProperty("line.separator"));
+            reader.lines().forEach(builder::add);
+            assertThat(builder.toString(), is("10:57:01;10:59:01\r\n11:01:02;11:02:02"));
+        }
+    }
+}
