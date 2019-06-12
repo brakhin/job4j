f4f946349546b6d6f91ff028ebe2241fa5ac0849
Boris Brakhin <mailto:brakhin@mail.ru>
2019-06-13 00:00:55 +0300

    2. Обеспечить остановку потребителя.[#147149]

---
+++ 3_multithread/src/main/java/ru/bgbrakhi/multithread/wait/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.bgbrakhi.multithread.wait;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+    }
+}
