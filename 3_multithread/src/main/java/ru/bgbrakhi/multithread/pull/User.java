f27fa0a201f410506c17858e4ffbea55f0c76e63
Boris Brakhin <brakhin@mail.ru>
2019-06-21 07:25:20 +0300

    2. ExecutorService рассылка почты.[#147145]

---
+++ 3_multithread/src/main/java/ru/bgbrakhi/multithread/pull/User.java
@@ -0,0 +1,19 @@
+package ru.bgbrakhi.multithread.pull;
+
+public class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
