6f6a6ba03ece20d8f413f3929878cb4f09013c7f
Boris Brakhin <brakhin@mail.ru>
2019-09-10 07:02:31 +0300

    2. Реализовать различные виды хранилищ.[#147087]

--- 3_spring_ioc/src/main/java/ru/bgbrakhi/spring/xml/Configuration.java
+++ 3_spring_ioc/src/main/java/ru/bgbrakhi/spring/xml/Configuration.java
@@ -1,15 +1,17 @@
 package ru.bgbrakhi.spring.xml;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Configuration implements IConfiguration {
     private final Properties config = new Properties();
 
+    @Autowired
     public Configuration() {
         try (InputStream in = getClass().getClassLoader().getResourceAsStream("app.properties")) {
             config.load(in);
-            int a = 0;
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
